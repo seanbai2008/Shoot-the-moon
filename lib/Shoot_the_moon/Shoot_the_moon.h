37d0d9767395f5c8d3117ca6119c15968432d1e8
Sean <seanbai2008@gmail.com>
2016-12-08 21:18:38 -0600

    open loop control realized. Trejactorys had been created including reaching from Earth to Pluto

--- lib/Shoot_the_moon/Shoot_the_moon.h
+++ lib/Shoot_the_moon/Shoot_the_moon.h
@@ -7,23 +7,31 @@
 
 class motor{
   //maximum steps for the motor to rotate before the rod touches the wall
-  const int16_t the_most_leftest_position = 266;
+  const int16_t the_most_leftest_position = 252;
   //maximum steps for the motor to rotate before the rod hit each other
   const int16_t the_most_rightest_position = 399;
   int16_t current_steps = 399;
 public:
 
+  //this function will initialize the coil voltage so that the motor will always
+  //starts with the same state
   void motor_init();
+
+  //the first strech of the rod should be as fast as possible, so this function
+  //is aimed to make the motor to rotate to the fisrt postion according to the
+  //trejactory as fast as possible
+  void initialize_position(int16_t inital_steps);
+
   // the function will leads the rods go to wall
   void make_single_forward_step();
-  // the function will leads the rods go to hit each other
+  // the function will leads the rods go to hit each otherssaa
   void make_single_backward_step();
 
   //this function allows user to input how many steps the motor needs to go
   void move(int16_t steps);
 
   //this function will return the current position of the motor
-  uint8_t get_current_position();
+  int16_t get_current_position(){return current_steps;}
   //this function will update the current position of the motor
   void set_current_position();
 
